41b2f4940d726b27dd7f92abf8632923e9b0c075
unknown <chayfree86@gmail.com>
2026-07-05 16:32:13 +0530

    update

--- api/db.php
+++ api/db.php
@@ -23,6 +23,7 @@ function db(): PDO
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES => false,
     ]);
+    $pdo->exec("SET NAMES 'utf8mb4' COLLATE 'utf8mb4_unicode_ci'");
     $pdo->exec("SET time_zone = '+05:30'");
 
     return $pdo;
